4576b95d334edbdee54ae8ecbd5b5cab606c017a
momondawi <58190417+momondawi@users.noreply.github.com>
2021-04-07 15:34:42 +0000

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,15 @@
+<?php
+
+    $score=$_REQUEST['hidden'];
+    $commentaire=$_REQUEST['Q17'];
+
+    try{
+        $bdd= new PDO('mysql:host=localhost;dbname=MeetCourseAnalyzer;charset=utf-8','root','passer', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }
+    catch(Exception $e){
+        die ('Erreur :'.$e->getMessage());
+    }
+
+    $req = $bdd->prepare('UPDATE Evaluation set noteEvaluation=?,commentaire=? where IDEvaluation=(select IDEvaluation from Evaluation order by IDEvaluation desc limit 1)');
+    $req->execute([$score,$commentaire]);
+?>
